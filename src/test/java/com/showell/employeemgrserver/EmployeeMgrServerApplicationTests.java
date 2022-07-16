2fdb07270275ba7cec172d2192609f46bee38272
Melony Showell <melony@Melonys-MacBook-Air.local>
2022-07-16 10:51:55 -0400

    idk

---
+++ src/test/java/com/showell/employeemgrserver/EmployeeMgrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showell.employeemgrserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
